29a197bd1deb55cfea0746b0ed34a3018b25d41c
uichi <37263474+uichi@users.noreply.github.com>
2023-10-15 18:43:18 +0900

    setup情報を更新

--- setup.py
+++ setup.py
@@ -43,8 +43,10 @@ setup(
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
     long_description_content_type='text/markdown',
-    long_description="""\
-    国税庁が提供するインボイス制度適格請求書発行事業者公表システムWeb-APIを使用するためのクライアントAPI(https://www.invoice-kohyo.nta.go.jp/web-api/index.html)
-    """,  # noqa: E501
+    long_description=open('README.md').read(),
     package_data={"kendama": ["py.typed"]},
+    project_urls={
+        'Documentation': 'https://github.com/uichi/kendama-python/blob/main/README.md',
+        'Source': 'https://github.com/uichi/kendama-python'
+    }
 )
